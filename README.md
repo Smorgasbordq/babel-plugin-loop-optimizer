dc8cba9ca763a4ef5d5e84da5507add5d3bc704f
smorgasbordq <abusdriverq@aim.com>
2020-04-05 15:14:06 -0400

    README update

--- README.md
+++ README.md
@@ -1,10 +1,127 @@
 # babel-plugin-loop-optimizer
 
-**WARNING:** This package is **unmaintained**. It probably still works but I am not able to maintain it. If anyone wishes to pick this up leave an issue and I can give access.
+Optimizes `.forEach`, `.every`, `.find`, `.map`, `.filter`, `.reduce`, `.reduceRight`, `.some` statements to `for` statements
+Arrow Expressions and inline functions are also altered to implement functionality directly in the loop, removing the need to craft and then invoke a function.
+Returns, for the most part, are changed into a combination of assignments and either continues; or breaks;
+Loops are conditonally labeled if required for altering a function.
+Also, ternary and logical operations are supported (`logicCheck() ? map : null` or `logicCheck() && map`).  An if statement will be inserted before the loop (`if(logicCheck())`), based on what needs to be evaluated.
+Because of this, ternary and logical operations should not have side effects, nor should they be computationally expensive (since they essentially need to be evaluated at least twice).
+Unlike the original babel-plugin-loop-optimizer, by default, for loops are evaluted from left-to-right. A comment of `//loop-optimizer: AGGRO` will use the more aggressive `while(iterator--)` methodology.
+This hasn't been posted to NPM yet.  I'm not sure what I'll do with this project.  The idea is to use it with JSX, where many renders with `.map` invocations can be optimized, especially for older browsers or node environments.
+The `lib_4-5-2020_index.js` is provided, if you want to use it in your project.
 
-Also, that package aren't working with newest Babel (that have @babel namespace) right now.
+#### Example benchmark output, based on Faster.js' benchmark
+```bash
+$ npm run bench
 
-Optimizes `.forEach`, `.every`, `.find`, `.map`, `.filter` statements to `for` stements
+    array-every small
+    ✓ native x 37,315,337 ops/sec ±2.60% (23 runs sampled)
+    ✓ loopOptimizer x 38,812,226 ops/sec ±0.21% (23 runs sampled)
+loopOptimizer is 4.0% faster (0.001μs) than native
+
+  array-every medium
+    ✓ native x 24,623,867 ops/sec ±0.36% (21 runs sampled)
+    ✓ loopOptimizer x 25,937,559 ops/sec ±0.42% (22 runs sampled)
+loopOptimizer is 5.3% faster (0.002μs) than native
+
+  array-every large
+    ✓ native x 22,135,948 ops/sec ±0.48% (22 runs sampled)
+    ✓ loopOptimizer x 24,276,020 ops/sec ±0.22% (22 runs sampled)
+loopOptimizer is 9.7% faster (0.004μs) than native
+
+  array-filter small
+    ✓ native x 24,574,208 ops/sec ±4.23% (23 runs sampled)
+    ✓ loopOptimizer x 25,505,463 ops/sec ±5.35% (20 runs sampled)
+loopOptimizer is 3.8% faster (0.001μs) than native
+
+  array-filter medium
+    ✓ native x 8,401,076 ops/sec ±6.72% (21 runs sampled)
+    ✓ loopOptimizer x 9,711,315 ops/sec ±7.62% (22 runs sampled)
+loopOptimizer is 15.6% faster (0.016μs) than native
+
+  array-filter large
+    ✓ native x 901,668 ops/sec ±3.18% (22 runs sampled)
+    ✓ loopOptimizer x 1,119,163 ops/sec ±5.01% (23 runs sampled)
+loopOptimizer is 24.1% faster (0.216μs) than native
+
+  array-forEach small
+    ✓ native x 17,466,552 ops/sec ±7.12% (20 runs sampled)
+    ✓ loopOptimizer x 20,589,787 ops/sec ±5.70% (20 runs sampled)
+loopOptimizer is 17.9% faster (0.009μs) than native
+
+  array-forEach medium
+    ✓ native x 3,463,584 ops/sec ±7.56% (20 runs sampled)
+    ✓ loopOptimizer x 3,705,916 ops/sec ±6.35% (21 runs sampled)
+loopOptimizer is 7.0% faster (0.019μs) than native
+
+  array-forEach large
+    ✓ native x 278,749 ops/sec ±21.37% (17 runs sampled)
+    ✓ loopOptimizer x 315,629 ops/sec ±33.02% (20 runs sampled)
+loopOptimizer is 13.2% faster (0.419μs) than native
+
+  array-map small
+    ✓ native x 9,420,233 ops/sec ±4.44% (19 runs sampled)
+    ✓ loopOptimizer x 33,183,281 ops/sec ±5.57% (22 runs sampled)
+loopOptimizer is 252.3% faster (0.076μs) than native
+
+  array-map medium
+    ✓ native x 5,013,457 ops/sec ±0.80% (22 runs sampled)
+    ✓ loopOptimizer x 12,792,167 ops/sec ±7.30% (21 runs sampled)
+loopOptimizer is 155.2% faster (0.121μs) than native
+
+  array-map large
+    ✓ native x 925,164 ops/sec ±3.99% (22 runs sampled)
+    ✓ loopOptimizer x 1,911,779 ops/sec ±3.35% (22 runs sampled)
+loopOptimizer is 106.6% faster (0.558μs) than native
+
+  array-reduce small
+    ✓ native x 37,610,932 ops/sec ±0.20% (22 runs sampled)
+    ✓ loopOptimizer x 40,082,314 ops/sec ±0.84% (23 runs sampled)
+loopOptimizer is 6.6% faster (0.002μs) than native
+
+  array-reduce medium
+    ✓ native x 15,253,030 ops/sec ±7.09% (21 runs sampled)
+    ✓ loopOptimizer x 18,676,404 ops/sec ±0.14% (18 runs sampled)
+loopOptimizer is 22.4% faster (0.012μs) than native
+
+  array-reduce large
+    ✓ native x 2,087,176 ops/sec ±0.13% (23 runs sampled)
+    ✓ loopOptimizer x 2,115,062 ops/sec ±0.23% (23 runs sampled)
+loopOptimizer is 1.3% faster (0.006μs) than native
+
+  array-reduceRight small
+    ✓ native x 35,768,410 ops/sec ±5.84% (22 runs sampled)
+    ✓ loopOptimizer x 38,743,982 ops/sec ±0.25% (21 runs sampled)
+loopOptimizer is 8.3% faster (0.002μs) than native
+
+  array-reduceRight medium
+    ✓ native x 16,103,549 ops/sec ±0.26% (22 runs sampled)
+    ✓ loopOptimizer x 16,352,026 ops/sec ±0.11% (22 runs sampled)
+loopOptimizer is 1.5% faster (0.001μs) than native
+
+  array-reduceRight large
+    ✓ native x 384,846 ops/sec ±117.26% (20 runs sampled)
+    ✓ loopOptimizer x 2,075,712 ops/sec ±0.37% (20 runs sampled)
+loopOptimizer is 439.4% faster (2.117μs) than native
+
+  array-some small
+    ✓ native x 37,360,221 ops/sec ±1.88% (22 runs sampled)
+    ✓ loopOptimizer x 38,720,416 ops/sec ±0.23% (21 runs sampled)
+loopOptimizer is 3.6% faster (0.001μs) than native
+
+  array-some medium
+    ✓ native x 23,660,916 ops/sec ±0.57% (21 runs sampled)
+    ✓ loopOptimizer x 26,117,993 ops/sec ±0.18% (21 runs sampled)
+loopOptimizer is 10.4% faster (0.004μs) than native
+
+  array-some large
+    ✓ native x 21,642,697 ops/sec ±0.28% (23 runs sampled)
+    ✓ loopOptimizer x 23,952,225 ops/sec ±1.25% (22 runs sampled)
+loopOptimizer is 10.7% faster (0.004μs) than native
+```
+
+The benchmark example above was run on Node 12, unlike Faster.js which reports findings from Node 8. I believe this project can offer vastly greater performance benefits to older runtime environments (IE, Node 8, etc).
+The rest of the README is leftover from the original project, except for the benchmark output. Once again, this project currently is not on NPM, but the lib_4-5-2020_index.js file is provided as a build to use.
 
 ## Installation
 
---
+++ lib_4-5-2020_index.js
@@ -0,0 +1,345 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+/**
+ * 
+ * @param {*} path Path to search
+ * @returns Statement Parent or Program
+ */
+function _findOrMakeStatementBlockAndGetOptions(t, incPath) {
+  var path = incPath;
+
+  do {
+    var par = path.parentPath,
+        rp = null,
+        isStmt = path.isStatement() || path.isProgram(),
+        comment;
+
+    if (isStmt) {
+      var comments = path.node.leadingComments;
+
+      if (comments && comments[comments.length - 1]) {
+        comment = comments[comments.length - 1].value;
+
+        if (/^\s*loop-optimizer:(\s+)KEEP/.test(comment)) {
+          return null;
+        }
+      }
+    }
+
+    if (par) switch (par.type) {
+      case "IfStatement":
+        rp = par.get("consequent");
+
+        if (!t.isBlockStatement(rp.node)) {
+          rp.replaceWith(t.blockStatement([rp.node]));
+        }
+
+        break;
+
+      case "ForStatement":
+      case "WhileStatement":
+        rp = par.get("body");
+
+        if (!t.isBlockStatement(rp.node)) {
+          rp.replaceWith(t.blockStatement([rp.node]));
+        }
+
+        break;
+
+      case "ArrowFunctionExpression":
+        rp = par.get("body");
+
+        if (!t.isBlockStatement(rp.node)) {
+          rp.replaceWith(t.blockStatement([t.returnStatement(rp.node)]));
+        }
+
+        break;
+    }
+
+    if (isStmt) {
+      return !comment ? [false, false] : [/^\s*loop-optimizer:(.*),?AGGRO/.test(comment), /^\s*loop-optimizer:(.*),?POSSIBLE_UNDEFINED/.test(comment)];
+    }
+  } while (path = path.parentPath);
+}
+/**
+ * Determine the logicalExpression needed to get to a path.
+ *
+ * @param path - The path that may or may not be the result of conditional/logical expressions.
+ * @returns Null, or a node or logically &&'d expressions of nodes to get to a path.
+ */
+
+
+function _logicalExpressionToPath(t, path) {
+  var par = path.parentPath,
+      oldNodeOfPath = path.node,
+      createdNode,
+      expr;
+
+  while (par) {
+    if (par.isStatement()) {
+      // was isBlockStatement || isVariableDeclarator
+      break;
+    }
+
+    if (par.isConditionalExpression() || par.isLogicalExpression()) {
+      createdNode = par.node;
+      /**/
+
+      if (createdNode.alternate === oldNodeOfPath) createdNode = t.unaryExpression("!", createdNode.test);else if (createdNode.consequent === oldNodeOfPath) createdNode = createdNode.test;else if (createdNode.right === oldNodeOfPath) createdNode = createdNode.operator === "&&" ? createdNode.left : createdNode.operator === "??" ? t.binaryExpression("==", createdNode.left, t.identifier("null")) : t.unaryExpression("!", createdNode.left);else createdNode = null; //else if(createdNode.test === oldNodeOfPath) createdNode = null;
+      //else if(createdNode.left === oldNodeOfPath) createdNode = null;
+
+      if (createdNode) {
+        expr = expr ? t.logicalExpression("&&", createdNode, expr) : createdNode;
+      }
+    }
+
+    oldNodeOfPath = par.node;
+    par = par.parentPath;
+  }
+
+  return expr;
+}
+/**
+ * Make an assignmentExpression (array[i]=value) and continueStatement (continue;) for a particular body.
+ * If required, returnModder is modified so that a labeledStatement is created at returnModder.lbl.
+ * @param returnModder - ReturnModder instance.
+ * @param body - The body, with an argument or expression to assign the array to.
+ * @param isLabelNeeded - If a label is needed, due to being within a Switch/For/While statement. 1===Needed For Break; 2===Needed For Continue/Break;
+ * @returns [] 0=expression(assignment("=",member)); 1=breakOrContinueStatement(withLabelIfRequired);
+ */
+
+
+function _craftAssignAndContinueExprs(returnModder, body, isLabelNeeded) {
+  var t = returnModder.t,
+      path = returnModder.path,
+      itemName = returnModder.itemName,
+      resArrName = returnModder.resArrName,
+      iterator = returnModder.iterator,
+      mode = returnModder.mode;
+
+  if (!returnModder.lbl && (isLabelNeeded === 2 || isLabelNeeded === 1 && (mode === 3 || mode === 4))) {
+    returnModder.lbl = t.labeledStatement(path.scope.generateUidIdentifier("l"), t.blockStatement([]));
+  }
+
+  var bodExpr = body.argument || body.expression || body;
+  var exprType = mode <= 1 ? 0 : bodExpr.type === "BooleanLiteral" ? bodExpr.value === false ? 1 : bodExpr.value === true ? 2 : 0 : 0;
+  var stmt = mode === 1 ? t.expressionStatement(t.assignmentExpression("=", t.memberExpression(resArrName, iterator, true), bodExpr)) : mode === 2 ? exprType === 1 ? null : exprType === 2 ? t.expressionStatement(t.callExpression(t.memberExpression(resArrName, t.identifier("push")), [itemName])) : t.ifStatement(bodExpr, t.expressionStatement(t.callExpression(t.memberExpression(resArrName, t.identifier("push")), [itemName]))) : mode === 3 ? exprType === 1 ? null : exprType === 2 ? t.expressionStatement(t.assignmentExpression("=", resArrName, itemName)) : t.ifStatement(bodExpr, t.blockStatement([t.expressionStatement(t.assignmentExpression("=", resArrName, itemName)), isLabelNeeded ? t.breakStatement(returnModder.lbl.label) : t.breakStatement()])) : mode === 4 ? exprType === 2 ? null : exprType === 1 ? t.expressionStatement(t.assignmentExpression("=", resArrName, t.booleanLiteral(false))) : t.ifStatement(t.unaryExpression("!", bodExpr), t.blockStatement([t.expressionStatement(t.assignmentExpression("=", resArrName, t.booleanLiteral(false))), isLabelNeeded ? t.breakStatement(returnModder.lbl.label) : t.breakStatement()])) : mode === 5 || mode === 6 ? t.expressionStatement(t.assignmentExpression("=", resArrName, bodExpr)) : mode === 7 ? exprType === 1 ? null : exprType === 2 ? t.expressionStatement(t.assignmentExpression("=", resArrName, t.booleanLiteral(true))) : t.ifStatement(bodExpr, t.blockStatement([t.expressionStatement(t.assignmentExpression("=", resArrName, t.booleanLiteral(true))), isLabelNeeded ? t.breakStatement(returnModder.lbl.label) : t.breakStatement()])) : null;
+  var next = mode === 3 && exprType === 2 || mode === 4 && exprType === 1 || mode === 6 && exprType === 2 ? isLabelNeeded ? t.breakStatement(returnModder.lbl.label) : t.breakStatement() : isLabelNeeded === 2 ? t.continueStatement(returnModder.lbl.label) : t.continueStatement();
+  return stmt && next ? [stmt, next] : stmt ? [stmt] : [next];
+}
+/**
+ * Modify return statements into array assignments and continues.
+ * @param returnModder - ReturnModder instance object: { t, path, itemName, resArrName, iterator, lbl, mode }
+ * @param body - The body, with an argument or expression to assign the array to.
+ * @param isLabelNeeded - If a label is needed, due to being within a Switch/For/While statement. 1===Needed For Break; 2===Needed For Continue/Break;
+ */
+
+
+function _modReturnsToAssignsAndContinues(returnModder, body, isLabelNeeded) {
+  var t = returnModder.t; // Hack to support body not being an array.
+
+  for (var i = 0, I = body.length || 1; i < I; i++) {
+    var bod = body[i] || body,
+        type = bod.type;
+
+    if (type === "ArrowFunctionExpression" || type === "FunctionExpression" || type === "FunctionDeclaration") {
+      continue;
+    }
+
+    var isLabelNeededForBod = isLabelNeeded === 2 || type === "ForStatement" || type === "WhileStatement" ? 2 : type === "SwitchStatement" ? 1 : isLabelNeeded;
+    var cons = void 0;
+
+    if (type === "ReturnStatement") {
+      if (body.length) {
+        // If possible, alter the array.
+        var res = _craftAssignAndContinueExprs(returnModder, bod, isLabelNeededForBod);
+
+        if (res.length === 1) {
+          body[i] = res[0];
+        } else {
+          body[i] = res[0];
+          body.splice(++i, 0, res[1]);
+          I++;
+        }
+      } else {
+        // Otherwise, use assign to change the body into a blockStatement. This shouldn't be reached though.
+        var _res = _craftAssignAndContinueExprs(returnModder, bod, isLabelNeededForBod);
+
+        Object.assign(body, _res.length === 1 ? _res[0] : t.blockStatement(_res));
+      }
+    } else if ((cons = bod.consequent) && cons.type === "ReturnStatement") {
+      var _res2 = _craftAssignAndContinueExprs(returnModder, cons, isLabelNeededForBod);
+
+      bod.consequent = _res2.length === 1 ? _res2[0] : t.blockStatement(_res2);
+    } else if (cons && cons.body) {
+      _modReturnsToAssignsAndContinues(returnModder, cons.body, isLabelNeededForBod);
+    } else if (cons && cons.length) {
+      _modReturnsToAssignsAndContinues(returnModder, cons, isLabelNeededForBod);
+    } else if (bod.cases) {
+      _modReturnsToAssignsAndContinues(returnModder, bod.cases, isLabelNeededForBod);
+    } else if (bod.body) {
+      _modReturnsToAssignsAndContinues(returnModder, bod.body, isLabelNeededForBod);
+    }
+  }
+}
+/**
+ * 
+ * @param {*} t babel t
+ * @param {*} path  babel path
+ * @param {*} optimize "Optimizes" by searching backwards from the end of the array
+ * @param {*} checkUndefined if a conditional is required, to check that the item !== undefined
+ * @param {*} mode 0=forEach, 1=map, 2=filter, 3=find, 4=every, 5=reduce, 6=reduceRight, 7=some
+ */
+
+
+function Handle_map(t, path, optimize, checkUndefined, mode) {
+  var func = path.node.arguments[0],
+      useArrExpr = path.node.callee.object.type === "Identifier",
+      arrayName = useArrExpr ? path.node.callee.object : path.scope.generateUidIdentifier("a"),
+      type = func.type,
+      isInFn = type === "ArrowFunctionExpression" || type === "FunctionExpression",
+      iterator = isInFn && func.params && func.params[mode === 5 || mode === 6 ? 2 : 1] || path.scope.generateUidIdentifier("i"),
+      len = path.scope.generateUidIdentifier("L"),
+      itemName = isInFn ? func.params && func.params[mode === 5 || mode === 6 ? 1 : 0] || mode >= 3 && path.scope.generateUidIdentifier("n") || null : path.scope.generateUidIdentifier("n"),
+      resArrName = mode > 0 ? (mode === 5 || mode === 6) && func.params && func.params[0] || path.scope.generateUidIdentifier("r") : null,
+      body = isInFn ? func.body.body || func.body : null,
+      lastOfBod = isInFn ? body.length ? body[body.length - 1] : body : null,
+      block = path.findParent(function (p) {
+    return p.isBlockStatement() || p.isProgram();
+  }),
+      startIf = _logicalExpressionToPath(t, path),
+      returnModder = {
+    t: t,
+    path: path,
+    itemName: itemName,
+    resArrName: resArrName,
+    iterator: iterator,
+    forState: forState,
+    mode: mode
+  };
+
+  if (isInFn) {
+    var itemDeclr = t.variableDeclaration("let", [t.variableDeclarator(itemName, t.memberExpression(arrayName, iterator, true))]);
+
+    if (!body.length) {
+      var assigns = _craftAssignAndContinueExprs(returnModder, lastOfBod, null);
+
+      var funcLiteralIgnore = func.body.type.lastIndexOf("Literal") >= 0;
+      func.body = t.blockStatement(assigns.length === 1 ? funcLiteralIgnore ? [] : [itemDeclr, func.body] : assigns[1].type === "BreakStatement" ? [itemDeclr, assigns[0], assigns[1]] : [itemDeclr, assigns[0]]);
+    } else {
+      if (itemName) {
+        body.unshift(itemDeclr);
+      }
+
+      if (mode > 0 && lastOfBod && (lastOfBod.expression || lastOfBod.argument)) {
+        var _exprs = _craftAssignAndContinueExprs(returnModder, lastOfBod, null);
+
+        body[body.length - 1] = _exprs[0];
+        if (_exprs.length === 2 && _exprs[1].type === "BreakStatement") body.push(_exprs[1]);
+      }
+
+      _modReturnsToAssignsAndContinues(returnModder, body, false);
+    }
+  }
+
+  var funcName = isInFn ? null : path.scope.generateUidIdentifier("f"),
+      expr;
+
+  if (isInFn) {
+    expr = func.body;
+  } else {
+    var calling = t.callExpression(funcName, mode === 5 || mode === 6 ? [resArrName, t.memberExpression(arrayName, iterator, true), iterator, arrayName] : [t.memberExpression(arrayName, iterator, true), iterator, arrayName]);
+
+    var _exprs2 = _craftAssignAndContinueExprs(returnModder, calling, null);
+
+    expr = _exprs2.length === 2 && _exprs2[1].type === "BreakStatement" ? t.blockStatement(_exprs2) : _exprs2[0];
+  }
+
+  var forState = optimize ? t.forStatement(t.variableDeclaration("let", [t.variableDeclarator(iterator, len)]), checkUndefined ? t.logicalExpression("&&", t.updateExpression("--", iterator), t.binaryExpression("!==", t.memberExpression(arrayName, iterator, true), t.identifier("undefined"))) : t.updateExpression("--", iterator), null, expr) : t.forStatement(t.variableDeclaration("let", [t.variableDeclarator(iterator, t.numericLiteral(0))]), checkUndefined ? t.logicalExpression("&&", t.binaryExpression("<", iterator, len), t.binaryExpression("!==", t.memberExpression(arrayName, iterator, true), t.identifier("undefined"))) : t.binaryExpression("<", iterator, len), t.updateExpression("++", iterator), expr); // Change the forState to be the labled for statement.
+
+  if (returnModder && returnModder.lbl) {
+    returnModder.lbl.body = forState;
+    forState = returnModder.lbl;
+  }
+
+  var exprs = [];
+
+  if (!useArrExpr) {
+    exprs.push(t.variableDeclaration("let", [t.variableDeclarator(arrayName, path.node.callee.object)]));
+  }
+
+  exprs.push(t.variableDeclaration("let", [t.variableDeclarator(len, t.memberExpression(arrayName, t.identifier('length')))]));
+  var resExpr = mode === 1 ? t.newExpression(t.identifier('Array'), [len]) : mode === 2 ? t.arrayExpression() : mode === 3 ? "\0" : mode === 4 ? t.booleanLiteral(true) : mode === 5 || mode === 6 ? path.node.arguments[1] || "\0" : mode === 7 ? t.booleanLiteral(false) : null;
+
+  if (resExpr) {
+    if (startIf) {
+      if (resExpr !== "\0") exprs.push(t.expressionStatement(t.assignmentExpression("=", resArrName, resExpr)));
+    } else {
+      exprs.push(t.variableDeclaration("let", [resExpr === "\0" ? t.variableDeclarator(resArrName) : t.variableDeclarator(resArrName, resExpr)]));
+    }
+  }
+
+  if (!isInFn) {
+    exprs.push(t.variableDeclaration("let", [t.variableDeclarator(funcName, path.node.arguments[0])]));
+  }
+
+  exprs.push(forState);
+
+  if (startIf) {
+    block.node.body.unshift(t.variableDeclaration("let", [t.variableDeclarator(resArrName)]));
+    path.getStatementParent().insertBefore([t.ifStatement(startIf, t.blockStatement(exprs))]);
+  } else {
+    path.getStatementParent().insertBefore(exprs);
+  }
+
+  var parentType = path.parentPath.type;
+
+  if (parentType === "ExpressionStatement") {
+    path.remove();
+  } else if (resArrName) {
+    path.replaceWith(resArrName);
+  } else {
+    path.replaceWith(t.identifier("undefined"));
+  }
+}
+
+var _default = function _default(babel) {
+  var t = babel.types;
+  var methods = {
+    forEach: 0,
+    map: 1,
+    filter: 2,
+    find: 3,
+    every: 4,
+    reduce: 5,
+    reduceRight: 6,
+    some: 7
+  };
+  return {
+    visitor: {
+      CallExpression: function CallExpression(path) {
+        if (path.node.callee.property) {
+          // && path.node.arguments.length === 1)
+          var method = methods[path.node.callee.property.name];
+
+          if (method !== undefined) {
+            var opts = _findOrMakeStatementBlockAndGetOptions(t, path);
+
+            if (opts) {
+              // Opts change to support find/reduce/reduceRight properly. Path changes due to arrowExpression.
+              if (method === 3 || method === 5) opts[0] = false;else if (method === 6) opts[0] = true;
+              Handle_map(t, path.node.arguments ? path : path.get("body.0.argument"), opts[0], opts[1], method);
+            }
+          }
+        }
+      }
+    }
+  };
+};
+
+exports["default"] = _default;
